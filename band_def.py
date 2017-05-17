2579c14399031eede116af34de9c87e8bc004b2e
ywl@XM <ywlbupt@163.com>
2017-05-17 14:18:46 +0800

    WCDMA sense begin

--- band_def.py
+++ band_def.py
@@ -26,11 +26,11 @@ class lte_band_cmw():
     def lte_channel_dl2ul(self, channel_num):
         return channel_num+self.dl_ul_offset[1]-self.dl_ul_offset[0]
 
-
-def lte_test_set(priority_band,priority_bw, ul_lmh=[1,1,1]):
+def lte_test_set(priority_band, priority_bw, test_set, ul_lmh=[1,1,1]):
     test_list=[]
+    bw_zip = list(zip(priority_bw,test_set))
     for band in priority_band :
-        for cmw_bw,bw_set in priority_bw :
+        for cmw_bw,bw_set in bw_zip :
             if band in bw_set:
                 # for i in range(3):
                 for i,v in enumerate(ul_lmh):
@@ -41,6 +41,16 @@ def lte_test_set(priority_band,priority_bw, ul_lmh=[1,1,1]):
                 # for ch_ul in band.bw_lmh_ul[cmw_bw]:
                     # test_list.append(ue_struct(band.band,ch_ul,band.lte_channel_ul2dl(ch_ul), cmw_bw))
     return test_list
+
+def wcdma_test_set(test_set, ul_lmh=[1,1,1]):
+    test_list=[]
+    for band_set in test_set:
+        for i,v in enumerate(ul_lmh):
+            if v:
+                test_list.append((band_set[0],band_set[1][i],band_set[2][i]))
+    return test_list
+
+
 
 lte_b1 = lte_band_cmw("OB1",[0   ,18000], {
     LTE_BW_10:(18050,18300,18550),
@@ -67,18 +77,23 @@ lte_b41_w = lte_band_cmw("OB41",[39650,39650], {LTE_BW_10:(39700,40620,41540), L
 # lte_5M = (lte_b3,lte_b1)
 lte_5M = ()
 lte_10M = (lte_b1,lte_b3,lte_b7,lte_b39,lte_b40,lte_b41_n)
-# lte_10M = (lte_b7,)
 #.format(down_level) lte_10M = (lte_b1,lte_b3,lte_b5,lte_b8,lte_b7)
 # lte_20M = (lte_b1,lte_b3,lte_b7)
 lte_20M = ()
 
-# priority_band = (lte_b1,lte_b3,lte_b5,lte_b8,lte_b7,lte_b39,lte_b40,lte_b41_n)
 priority_band = (lte_b1,lte_b3,lte_b5,lte_b8,lte_b7,lte_b39,lte_b40,lte_b41_n)
 # 使用zip返回一个生成器
-priority_bw = list(zip((LTE_BW_10,LTE_BW_20,LTE_BW_5),(lte_10M,lte_20M,lte_5M)))
+priority_bw = (LTE_BW_10,LTE_BW_20,LTE_BW_5)
 
 
-TEST_LIST = lte_test_set(priority_band,priority_bw, ul_lmh=[0,1,0])
+w_b1 = ("OB1", [9612, 9750, 9888],[10562,10700,10838])
+w_b2 = ("OB2", [9262, 9400, 9538],[9662, 9800, 9938])
+w_b5 = ("OB5", [4132, 4183, 4233],[4357,4408,4458])
+w_b8 = ("OB8", [2712, 2788, 2863],[2937,3013,3088])
+
+TEST_LIST = lte_test_set(priority_band, priority_bw, (lte_10M, lte_20M, lte_5M), ul_lmh=[1,1,0])
+TEST_LIST_W = wcdma_test_set((w_b1, w_b2, w_b5, w_b8),ul_lmh=[1,1,1])
+
 
 if __name__ == "__main__":
     # a=lte_b8
@@ -86,5 +101,7 @@ if __name__ == "__main__":
     # print(a.lte_channel_dl2ul(50))
     # print(a.channel_raster)
     for i in TEST_LIST:
+        print(i)
+    for i in TEST_LIST_W:
         print(i)
     pass
--- test.py
+++ test.py
@@ -3,16 +3,8 @@
 
 import sys, os, visa, threading, time, string
 from band_def import TEST_LIST
+from band_def import TEST_LIST_W
 from band_def import str_ue_info
-from band_def import lte_b1
-from band_def import lte_b3
-from band_def import lte_b5
-from band_def import lte_b8
-from band_def import lte_b7
-from band_def import lte_b39
-from band_def import lte_b38
-from band_def import lte_b40
-from band_def import lte_b41_n
 from MACRO_DEFINE import *
 from adb import adb
 
@@ -34,30 +26,36 @@ class handle_instr():
         self.phone_hd=phone_hd
 
     def instr_write(self, *args, **kwargs):
-        self.instr.write(*args, **kwargs)
+        try:
+            self.instr.write(*args, **kwargs)
+        except:
+            print("write error ",args)
+            time.sleep(4)
+            self.instr.write(*args, **kwargs)
 
     def instr_query(self, *args, **kwargs):
         try:
             m = self.instr.query(*args,**kwargs)
         # return self.instr.query(cmd)
         except:
-            print("query error")
-            time.sleep(2)
+            print("query error {0}",args)
+            time.sleep(4)
             m = self.instr.query(*args,**kwargs)
         return m
 
     def instr_reset_cmw(self):
-        self.instr.write("*RST;*OPC")
-        self.instr.write("*CLS; *OPC?")
+        self.instr_write("*RST;*OPC")
+        self.instr_write("*CLS;*OPC?")
         # preset instr
-        self.instr.write("SYSTem:PRESet:ALL")
+        self.instr_write("SYSTem:PRESet:ALL;*OPC")
+        # self.instr_write("SYSTem:PRESet:ALL")
         time.sleep(5)
 
     def instr_close(self):
         self.instr.close()
 
     def get_instr_version(self):
-        return self.instr_query("*IDN?",delay = 3)
+        return self.instr_query("*IDN?",delay = 5).strip()
 
     def set_remote_display(self, state=True):
         if state:
@@ -73,14 +71,19 @@ class handle_instr():
         self.instr_write ("CONFigure:FDCorrection:ACTivate RF1C, 'CMW_loss', RXTX, RF1")
         self.instr_write ("CONFigure:FDCorrection:ACTivate RF1O, 'CMW_loss', RXTX, RF1")
 
-    def LTE_para_configure(self,state="Disconnected"):
-        if state=="Disconnected":
+    def LTE_para_configure(self,test_list):
+        if self.LW_check_connection(md="LTE"):
+            self.instr_write("CONFigure:LTE:SIGN:DL:RSEPre:LEVel -80")
+            self.LTE_ch_redirection(test_list[0])
+            pass
+        else:
+            self.instr_reset_cmw()
             self.instr_write ("SOURce:LTE:SIGN:CELL:STATe OFF")
-            self.instr_write ("CONFigure:LTE:SIGN:DMODe FDD")
-            self.instr_write ("CONFigure:LTE:SIGN:PCC:BAND OB1")
-            self.instr_write ("CONFigure:LTE:SIGN:RFSettings:CHANnel:UL 18050")
+            test_DD = "FDD" if int(test_list[0].BAND[2:])<33 else "TDD"
+            self.instr_write ("CONFigure:LTE:SIGN:DMODe {DD}".format(DD=test_DD))
+            self.instr_write ("CONFigure:LTE:SIGN:PCC:BAND {band}".format(band=test_list[0].BAND))
+            self.instr_write ("CONFigure:LTE:SIGN:RFSettings:CHANnel:UL {0}".format(test_list[0].CH_UL))
         # elif state =="Connected":
-        self.instr_write("CONFigure:LTE:SIGN:DL:RSEPre:LEVel -80")
         # 最大功率
         self.instr_write ("CONFigure:LTE:SIGN:UL:PUSCh:TPC:SET MAXPower")
         self.instr_write ("SOURce:LTE:SIGN:CELL:STATe ON")
@@ -90,24 +93,44 @@ class handle_instr():
             time.sleep(1)
         return 
 
-    def Check_connection(self):
-        if self.instr_query("FETCh:LTE:SIGN:PSWitched:STATe?").strip() == "CEST":
+    def LW_check_connection(self, md = "LTE"):
+        if md == "LTE":
+            str_query = "FETCh:LTE:SIGN:PSWitched:STATe?"
+        elif md == "WCDMA":
+            str_query = "FETCh:WCDMa:SIGN:CSWitched:STATe?"
+        str_state_conn = "CEST"
+
+        if self.instr_query(str_query,delay=2).strip() == str_state_conn:
             return True
         else:
             return False
 
-    def LTE_Connect(self, data_ue_struct=None, phone_reset=False, instr_reset=False):
+    def LW_connect(self,md="LTE", phone_reset=False, instr_reset=False):
         if instr_reset:
             self.instr_reset_cmw()
             self.set_FDCorrection(param_FDCorrection)
         if self.phone_hd and phone_reset:
             self.phone_hd.reboot()
+        if md == "LTE":
+            str_call = "CALL:LTE:SIGN:PSWitched:ACTion CONNect"
+            str_query = "FETCh:LTE:SIGN:PSWitched:STATe?"
+            str_state_conn = "CEST"
+        elif md == "WCDMA":
+            str_call = "CALL:WCDMa:SIGN:CSWitched:ACTion CONNect"
+            str_query = "FETCh:WCDMa:SIGN:CSWitched:STATe?"
+            str_state_conn = "CEST"
+        print("{md} connect begin".format(md = md))
         for j in range(2):
             for i in range(50):
-                self.instr_write("CALL:LTE:SIGN:PSWitched:ACTion CONNect")
-                time.sleep(1)
-                res_state = self.instr_query("FETCh:LTE:SIGN:PSWitched:STATe?").strip()
-                if res_state != "CEST":
+                if md == "LTE":
+                    self.instr_write(str_call)
+                    res_state = self.instr_query(str_query,delay=1).strip()
+                elif md == "WCDMA":
+                    res_state = self.instr_query(str_query,delay=1).strip()
+                    if res_state == "REG":
+                        self.instr_write(str_call)
+
+                if res_state != str_state_conn:
                     print("\r{0:<5} ".format(res_state),end="")
                     print("Connecting phone, {0}".format(i),end="")
                 else:
@@ -122,7 +145,7 @@ class handle_instr():
                     # time.sleep(2)
             else:
                 break
-        if res_state == "CEST":
+        if res_state == str_state_conn:
             print("Connection Established!")
         else:
             print("Conecting failed")
@@ -196,12 +219,7 @@ class handle_instr():
             print(total_res)
         pass
 
-        # output_result = {}
-        # for item in mea_item:
-            # output_result[item] = []
-
     def LTE_acquire_Meas(self, mea_item = None, test_DD="FDD"):
-        # 设置默认参数
         if test_DD == "FDD":
             self.instr_write("CONFigure:LTE:MEAS:MEValuation:MSUBframes 0,10,0")
         else:
@@ -210,7 +228,7 @@ class handle_instr():
         output_res = {}
         if not mea_item:
             mea_item = ("aclr",)
-        if self.Check_connection():
+        if self.LW_check_connection(md="LTE"):
             self.instr_write("INITiate:LTE:MEAS:MEValuation")
             while self.instr_query("FETCh:LTE:MEAS:MEValuation:STATe:ALL?").strip() != "RDY,ADJ,INV":
                 # print (self.instr_query("FETCh:LTE:MEAS:MEValuation:STATe:ALL?"))
@@ -226,10 +244,9 @@ class handle_instr():
             print("Not connected")
         return output_res
 
-
     def LTE_meas_aclr(self):
         self.instr_write("CONFigure:LTE:SIGN:DL:RSEPre:LEVel -80")
-        self.instr_write ("CONFigure:LTE:SIGN:UL:PUSCh:TPC:SET MAXPower")
+        self.instr_write("CONFigure:LTE:SIGN:UL:PUSCh:TPC:SET MAXPower")
         time.sleep(1)
         res = self.LTE_data_dealing(self.instr_query ("READ:LTE:MEAS:MEValuation:ACLR:AVERage?"), data_type="aclr")
         print(res)
@@ -269,7 +286,6 @@ class handle_instr():
         # ue_struct = namedtuple("ue_struct",['BAND','CH_UL','CH_DL','BW'])
         return ue_struct(band, int(ch_ul),int(ch_dl), bw)
 
-    # EBL : EBLer
     def LTE_meas_sense(self,route_path="main", pwr_init =-120, pwr_coarse = 0.5, pwr_fine=0.1, frame_coarse=200, frame_fine = 1000):
         self.instr_write("CONFigure:LTE:SIGN:EBLer:REPetition SING")
         self.instr_write("CONFigure:LTE:SIGN:EBLer:SCONdition NONE")
@@ -380,13 +396,149 @@ class handle_instr():
                         f.write("\t{0}".format(output_result[item][i][4]))
                     f.write("\n")
 
-    def test(self):
-        self.instr_reset_cmw()
+    def LW_sense_algorithm(self,md="LTE"):
+        if md == "LTE":
+            pwr_init = -120
+            pwr_coarse = 0.5
+            pwr_fine = 0.1
+            frame_coarse = 200
+            frame_fine = 1000
+            BER_threshold = 5
+
+        
+        # init, coarse, pwr_back, fine, pwr_back_fine, end
+
+    def WCDMA_para_configure(self, test_list = None):
+        if test_list is None:
+            test_list = (("OB1", 9612, 10562),)
+
+        if self.LW_check_connection(md = "WCDMA"):
+            self.WCDMA_ch_redirection(test_list[0])
+        else:
+            self.instr_reset_cmw()
+            self.set_FDCorrection(param_FDCorrection)
+            self.instr_write("SOURce:WCDMa:SIGN:CELL:STATe OFF")
+            self.instr_write("CONFigure:WCDMa:SIGN:CARRier:BAND {0}".format(test_list[0][0]))
+            self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:UL {0}".format(test_list[0][1]))
+            self.instr_write("CONFigure:WCDMa:SIGN:CONNection:TMODe:RMC:TMODe MODE2")
+            self.instr_write("SOURce:WCDMa:SIGN:CELL:STATe ON")
+            self.instr_write("ROUTe:WCDMa:MEAS:SCENario:CSPath 'WCDMA Sig1'")
+            while self.instr_query("SOUR:WCDM:SIGN:CELL:STAT:ALL?").strip()!="ON,ADJ":
+                time.sleep(1)
+            print("Cell initialling done")
+        self.instr_write("CONFigure:WCDMa:MEAS:MEValuation:REPetition SINGleshot")
+        self.instr_write("CONFigure:WCDMa:MEAS:MEValuation:SCOunt:SPECtrum 10")
+        self.instr_write("CONFigure:WCDMa:MEAS:MEValuation:SCOunt:MODulation 10")
+        self.instr_write("CONFigure:WCDMa:SIGN:UL:TPC:SET ALL1")
+
+    def WCDMA_ch_travel(self, test_list,mea_item=("aclr",)):
+        # self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:COPower -56.10")
+        # 平均次数
+        if "sensm" in mea_item:
+            self.instr_write("CONFigure:WCDMa:SIGN:BER:SCONdition None")
+            self.instr_write("CONFigure:WCDMa:SIGN:BER:REPetition SINGleshot")
+            self.instr_write("CONFigure:WCDMa:SIGN:BER:TBLocks 100")
+        for item in test_list:
+            self.WCDMA_ch_redirection(item)
+            self.WCDMA_acquire_Meas(mea_item = mea_item)
+
+    def WCDMA_acquire_Meas(self, mea_item=("aclr",)):
+        if self.LW_check_connection(md="WCDMA"):
+            self.instr_write("INITiate:WCDMa:MEAS:MEValuation")
+            while self.instr_query("FETCh:WCDMa:MEAS:MEValuation:STATe:ALL?").strip() != "RDY,ADJ,INV":
+                time.sleep(1)
+            if "aclr" in mea_item:
+                self.WCDMA_meas_aclr()
+            if "sensm" in mea_item:
+                self.WCDMA_meas_sense()
+            print("")
+        else:
+            print("Not connected")
+        pass
+
+    def WCDMA_meas_aclr(self):
+        self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:COPower -56.10")
+        self.instr_write("CONFigure:WCDMa:SIGN:UL:TPC:SET ALL1")
+        time.sleep(1)
+        res = self.instr_query("READ:WCDMa:MEAS:MEValuation:SPECtrum:AVERage?",delay = 4)
+        # res = self.instr_query("FETCh:WCDMa:MEAS:MEValuation:SPECtrum:AVERage?")
+        res = [ round(float(res.split(",")[i]),2) for i in [2,3,15,4,5] ]
+        # return (round(res[2],2),round(res[3],2),round(res[15],2),round(res[4],2),round(res[5],2))
+        print(res)
+        return res
+
+    def WCDMA_meas_sense(self):
+        print(self.WCDMA_meas_sense_cell(-90))
+        self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:COPower -56.1")
+        pass
+
+    def WCDMA_meas_sense_cell(self, down_level):
+        self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:COPower {0}".format(down_level))
+        time.sleep(1)
+        # self.instr_write("INITiate:WCDMa:SIGN:BER")
+        res = self.instr_query("READ:WCDMa:SIGN:BER?",delay = 4)
+        return down_level, round(float(res.split(",")[1]),2)
+
+    def WCDMA_ch_redirection(self,dest_state):
+        self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:COPower -56.10")
+        last_state = self.WCDMA_get_state()
+        print("try redrection to {0}".format(dest_state))
+        if last_state[0] == dest_state[0]:
+            if last_state[1] != dest_state[1]:
+                self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:UL {ch_ul}".format(ch_ul=dest_state[1]))
+                time.sleep(4)
+        else:
+            self.instr_write("CONFigure:WCDMa:SIGN:CARRier:BAND {band}".format(band=dest_state[0]))
+            time.sleep(8)
+            self.instr_write("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:UL {ch_ul}".format(ch_ul=dest_state[1]))
+            time.sleep(4)
+        if self.LW_check_connection(md="WCDMA"):
+            if self.WCDMA_get_state() == dest_state:
+                print("redirection successful")
+            else:
+                print("redirection failed but connected")
+        else:
+            print("------------redirection error---------------------------")
+
+    def WCDMA_get_state(self):
+        band = self.instr_query("CONFigure:WCDMa:SIGN:CARRier:BAND?").strip()
+        ch_ul = self.instr_query("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:UL?").strip()
+        ch_dl = self.instr_query("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:DL?").strip()
+        return (band, int(ch_ul), int(ch_dl))
+
+        # "SOURce:WCDMa:SIGN:CELL:STATe ON"
+        # ON 的过程比较久
+        # PM.write ("CONFigure:WCDMa:SIGN:CARRier:BAND OB1")
+        # PM.write ("CONFigure:WCDMa:SIGN:RFSettings:CARRier:CHANnel:UL 9612")
+        # "ROUTe:WCDMa:MEAS:SCENario:CSPath 'WCDMA Sig1'"
+        # CONFigure:WCDMa:SIGN:CONNection:TMODe:RMC:TMODe MODE2
+        #     PM.write ("CONFigure:WCDMa:SIGN:UL:TPC:SET ALL1")
+        # CONFigure:WCDMa:SIGN:RFSettings:COPower -56.10
+
+
+        # CONFigure:WCDMa:SIGN<i>:CARRier<c>:BAND
+        # CONFigure:WCDMa:SIGN<i>:RFSettings:CARRier<c>:CHANnel:DL
+        # CONFigure:WCDMa:SIGN<i>:RFSettings:CARRier<c>:CHANnel:UL
+        # 查看WCDMA的软件版本
+        # SYST:OPT:VERS? 'CMW_WCDMA_Sig'
+
+    def test_wcdma(self):
+        # self.instr_reset_cmw()
         # base 配置 loss
         self.set_FDCorrection(param_FDCorrection)
-        # self.LTE_para_configure(state="Connected")
-        self.LTE_para_configure(state="Disconnected")
-        self.LTE_Connect()
+        # for i in range(10):
+        # print("----------------- {0} time -----------".format(i))
+        self.WCDMA_para_configure(TEST_LIST_W)
+        self.LW_connect(md="WCDMA")
+        self.WCDMA_ch_travel(TEST_LIST_W, mea_item = ("sensm","aclr"))
+        pass
+    
+    def test_lte(self):
+        # self.instr_reset_cmw()
+        # base 配置 loss
+        self.set_FDCorrection(param_FDCorrection)
+        self.LTE_para_configure(TEST_LIST)
+        self.LW_connect(md="LTE")
         self.LTE_ch_travel(TEST_LIST,mea_item=("aclr","sensm","sensd"))
         # self.LTE_ch_travel(TEST_LIST,mea_item=("aclr","sensm"))
         pass
@@ -408,7 +560,8 @@ if __name__ == '__main__':
 
     print(m.get_instr_version())
     m.set_remote_display(state=True)
-    m.test()
+    m.test_lte()
+    # m.test_wcdma()
 
     m.instr_close()
     rm.close()
